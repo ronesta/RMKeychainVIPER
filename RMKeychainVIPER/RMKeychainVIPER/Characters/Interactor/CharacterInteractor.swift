e85c866955886e0098c60bd51688985df89b7545
ronesta <gvozzzd123@gmail.com>
2025-02-28 17:41:05 +0300

    Edits

--- RMKeychainVIPER/RMKeychainVIPER/Characters/Interactor/CharacterInteractor.swift
+++ RMKeychainVIPER/RMKeychainVIPER/Characters/Interactor/CharacterInteractor.swift
@@ -6,26 +6,40 @@
 //
 
 import Foundation
+import UIKit.UIImage
 
 final class CharacterInteractor: CharacterInteractorProtocol {
-    var presenter: CharacterPresenterProtocol?
-    var networkManager: NetworkManagerProtocol?
-    var storageManager: StorageManagerProtocol?
+    private let presenter: CharacterPresenterProtocol
+    private let networkManager: NetworkManagerProtocol
+    private let storageManager: StorageManagerProtocol
+
+    init(presenter: CharacterPresenterProtocol,
+         networkManager: NetworkManagerProtocol,
+         storageManager: StorageManagerProtocol
+    ) {
+        self.presenter = presenter
+        self.networkManager = networkManager
+        self.storageManager = storageManager
+    }
 
     func getCharacters() {
-        if let savedCharacters = storageManager?.loadCharacters() {
-            presenter?.charactersFetched(savedCharacters)
+        if let savedCharacters = storageManager.loadCharacters() {
+            presenter.charactersFetched(savedCharacters)
             return
         }
 
-        networkManager?.getCharacters { [weak self] result in
+        networkManager.getCharacters { [weak self] result in
             switch result {
             case .success(let characters):
-                self?.storageManager?.saveCharacters(characters)
-                self?.presenter?.charactersFetched(characters)
+                self?.storageManager.saveCharacters(characters)
+                self?.presenter.charactersFetched(characters)
             case .failure(let error):
-                self?.presenter?.charactersFetchFailed(with: error)
+                self?.presenter.charactersFetchFailed(with: error)
             }
         }
+    }
+
+    func loadImage(for character: Character, completion: @escaping (UIImage?) -> Void) {
+        networkManager.loadImage(from: character.image, completion: completion)
     }
 }
